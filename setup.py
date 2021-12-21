c0c5a6f030281efc260d19bb5d7e467e51278465
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2021-12-21 12:14:04 -0300

    updated setup.py author info

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='resetmigrations',
     version="0.0.2",
     packages=find_packages(),
+    email='jcmacielhenning@gmail.com',
+    author='Juan Cruz Maciel Henning',
     install_requires=['click', 'rich'],
     entry_points='''
     [console_scripts]
